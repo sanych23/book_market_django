8f9a3f51134b928d85a7a4eebf69780fa28e1e67
Sasha <asanochkin23@mail.ru>
2024-07-14 22:00:50 +0300

    Add unit test for CRUD operation in my API

--- book_market/store/tests/test_api.py
+++ book_market/store/tests/test_api.py
@@ -3,10 +3,13 @@ from rest_framework import status
 from django.urls import reverse
 from store.serializers import BooksSerializer
 from store.models import Book
+from django.contrib.auth.models import User
+import json
 
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(name='Test Book 1', price=25, author_name="Author 1")
         self.book_2 = Book.objects.create(name='Test Book 2', price=55, author_name="Author 5")
         self.book_3 = Book.objects.create(name='Test Book Author 1', price=55, author_name="Author 2")
@@ -38,4 +41,60 @@ class BooksApiTestCase(APITestCase):
         serializer_data = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer_data, response.data)
+
+    def test_get_one(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            'id': self.book_1.id,
+            'name': self.book_1.name,
+            'price': '25.00',
+            'author_name': self.book_1.author_name
+        }
+        self.client.force_login(self.user)
+        # self.assertEqual(Book.objects.all().count(), 3)
+        response = self.client.get(url, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, response.data)
     
+    def test_create(self):
+        self.assertEqual(Book.objects.all().count(), 3)
+        url = reverse('book-list')
+        data = {
+            "name": "Python 3",
+            "price": 150,
+            "author_name": "Mark Summerfield"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.all().count(), 4)
+        data = {
+            "id": 4,
+            "name": "Python 3",
+            "price": '150.00',
+            "author_name": "Mark Summerfield"
+        }
+        self.assertEqual(data, response.data)
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book_1.refresh_from_db()
+        self.assertEqual(575, self.book_1.price)
+
+    def test_delete(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        self.client.force_login(self.user)
+        self.assertEqual(Book.objects.all().count(), 3)
+        response = self.client.delete(url, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.all().count(), 2)
--- book_market/store/views.py
+++ book_market/store/views.py
@@ -4,7 +4,7 @@ from store.serializers import BooksSerializer
 from store.models import Book
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 # Create your views here.
@@ -12,7 +12,7 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
