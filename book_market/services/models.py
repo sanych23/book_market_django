5acbdc2ddeb24bb8f1fed08e744955d4fa6fb5ba
Sasha <asanochkin23@mail.ru>
2024-08-15 13:46:53 +0300

    make paralel tasks

--- book_market/services/models.py
+++ book_market/services/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 from clients.models import Client
-from services.tasks import set_price
+from services.tasks import set_comment, set_price
 
 
 # Create your models here.
@@ -42,6 +42,7 @@ class Plan(models.Model):
         if self.discount_percent != self.__discount_percent:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
 
         return super().save(*args, **kwargs)
     
@@ -54,6 +55,7 @@ class Subscription(models.Model):
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default='')
 
     # def save(self, *args, save_model=True, **kwargs):
     #     if save_model:
